5d7e2f2f4a01d6cb7b0ba690c082eed5d429e7fd
H4M5TER <zhuxx2003@gmail.com>
2023-01-17 18:55:06 +0800

    update plugin list

--- recomm/plugins/trim.js
+++ recomm/plugins/trim.js
@@ -2,16 +2,26 @@ const { writeFileSync } = require('fs')
 const { objects } = require('./index.json')
 const keys = ['name', 'links', 'keywords', 'portable', 'insecure']
 let list = objects.map(v => {
-  return Object.fromEntries(keys.map(k => [k, v.package[k]]))
+  let res = Object.fromEntries(keys.map(k => [k, v.package[k]]))
+  res.ignored = v.ignored ?? false
+  res.verified = v.verified ?? false
+  return res
 }).filter((v) => {
-  if (!v.keywords)
-    console.log(v)
-  return !v.keywords?.includes('market:hidden')
-})
-  .sort((a, b) => a.name > b.name ? 1 : -1)
+  if (v.keywords?.includes('market:hidden'))
+    return false
+  if (v.ignored)
+    return false
+  return true
+}).sort((a, b) => a.name > b.name ? 1 : -1)
+
 writeFileSync('./recomm/plugins/index_trim.json', JSON.stringify(list, null, 2), 'utf-8')
 writeFileSync('./recomm/plugins/index.md',
   list.map(v => {
     let name = v.name.replace(/(koishi-|@koishijs\/)plugin-/, '')
-    return `- [${name}](${v.links.npm})${v.name.includes('@koishijs/') ? ' (官方)' : ''}`
+    let res = `- ${name} [![评分](https://badge.koishi.chat/rating/${v.name})](${v.links.npm})`
+    if (v.name.includes('@koishijs/') || v.verified)
+      res += ' ![受认证|16x16](upload://nEu5NpYObSUxSNq2YszbREIgDoE.png)'
+    // if (v.insecure) 已有颜色区分
+    //   res += ' ![不安全|16x16](upload://puVx64WRItNaep7Ta8zFV3oa4Q2.png)'
+    return res
   }).join('\n'), 'utf-8')
